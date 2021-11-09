31abf6d754af176e6c967de1f4e43752774e7586
unknown <sam.plant998@gmail.com>
2021-11-09 19:29:17 +0000

    Lab 1 added

---
+++ Lab 1/Lab_Library 1/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Library_1
+{
+    class Book
+    {
+        private string title;
+        private Person author;
+
+        public Book(string title)
+        {
+            this.title = title;
+        }
+
+        public Book(string title, Person author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public Person Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string GetSummary()
+        {
+            string summary = "Title: " + title + "  Author: " + author.Name+"  Age: " + author.Age;
+            return summary;
+        }
+    }
+}
---
+++ Lab 1/Lab_Library 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Library_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[3];
+
+            books[0] = new Book("Moby Dick");
+
+            books[0].Author = new Person("Herman Melville");
+
+            books[0].Author.Age = 53;
+
+            books[1] = new Book("The Creeping");
+
+            books[1].Author = new Person("Luke D");
+
+            books[1].Author.Age = 43;
+
+            books[2] = new Book("To Kill A Mockingbird");
+
+            books[2].Author = new Person("Harper Lee");
+
+            books[2].Author.Age = 90;
+
+            for (int i = 0; i<3; i++)
+            {
+                Console.WriteLine(books[i].GetSummary());
+
+            }
+
+            if (books[0].Title.CompareTo(books[1].Title) < 0)
+            {
+                Console.WriteLine("Title of book in position 0 precedes (alphabetically) the title of " + books[1].Title + " book in position 1");
+            }
+            else if (books[0].Title.CompareTo(books[1].Title) > 0)
+            {
+                Console.WriteLine("Title of book in position 1 precedes (alphabetically) the title of " + books[0].Title + " book in position 0");
+            }
+            else if (books[0].Title.CompareTo(books[1].Title) == 0)
+            {
+                Console.WriteLine("Title of book in position 1 is the same as the title of " + books[0].Title + " book in position 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
